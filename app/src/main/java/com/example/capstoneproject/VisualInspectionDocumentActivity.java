0d6f1c06ef029da619d18a70bcf091ce0ed98859
Plath <cooper.plath@gmail.com>
2023-09-25 15:03:29 +1000

    Began work on VisualInspectionDocumentActivity, completed xml with textviews and checkboxes to match template

---
+++ app/src/main/java/com/example/capstoneproject/VisualInspectionDocumentActivity.java
@@ -0,0 +1,14 @@
+package com.example.capstoneproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class VisualInspectionDocumentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_visual_inspection_document);
+    }
+}
